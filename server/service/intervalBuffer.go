fc53e61f46acc72e3394341f50bc8b6027d0ef29
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2018-08-06 17:06:18 +0200

    Added an Average() function to IntervalBuffer

--- server/service/intervalBuffer.go
+++ server/service/intervalBuffer.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"math"
 	"sync"
 	"time"
 )
 
+// IntervalBuffer is a memory structure that you can add numbers to,
+// and it "forgets" them after a certain amount of time.
+// This is useful for counting items per minute, producing producing moving averages, etc.
 type IntervalBuffer struct {
 	interval time.Duration
 	buffer   []intervalBufferElement
@@ -17,6 +21,8 @@ type intervalBufferElement struct {
 	timestamp time.Time
 }
 
+// NewIntervalBuffer creates a new IntervalBuffer that operates
+// on the given interval of time
 func NewIntervalBuffer(_interval time.Duration) *IntervalBuffer {
 	return &IntervalBuffer{
 		interval: _interval,
@@ -25,10 +31,12 @@ func NewIntervalBuffer(_interval time.Duration) *IntervalBuffer {
 	}
 }
 
+// Add another value to the buffer, using the current time as the timestamp
 func (ma *IntervalBuffer) Add(newValue float64) {
 	ma.AddT(newValue, time.Now())
 }
 
+// AddT adds another value to the buffer, with the given timestamp
 func (ma *IntervalBuffer) AddT(newValue float64, when time.Time) {
 	ma.mu.Lock()
 	defer ma.mu.Unlock()
@@ -51,6 +59,8 @@ func (ma *IntervalBuffer) AddT(newValue float64, when time.Time) {
 	ma.buffer = append(ma.buffer, newElement)
 }
 
+// Sum returns the sum of all numbers in the buffer with a timestamp
+// not older than the buffer's interval
 func (ma *IntervalBuffer) Sum() float64 {
 	ma.mu.Lock()
 	defer ma.mu.Unlock()
@@ -62,4 +72,24 @@ func (ma *IntervalBuffer) Sum() float64 {
 		}
 	}
 	return sum
+}
+
+// Average returns the average of all numbers in the buffer with a timestamp
+// not older than the buffer's interval
+func (ma *IntervalBuffer) Average() float64 {
+	ma.mu.Lock()
+	defer ma.mu.Unlock()
+	var sum float64
+	count := 0
+	now := time.Now()
+	for _, elem := range ma.buffer {
+		if now.Sub(elem.timestamp) <= ma.interval {
+			sum += elem.value
+			count++
+		}
+	}
+	if count == 0 {
+		return math.NaN()
+	}
+	return sum / float64(count)
 }
