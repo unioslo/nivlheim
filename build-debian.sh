96a493e579bb79a98a92e178b19a1dab91c3e129
Andreas Dobloug <andreasd@usit.uio.no>
2019-03-21 07:27:09 +0100

    Change version number to comply with source format, and build in a separate directory. We should be using pristine-tar.

--- build-debian.sh
+++ build-debian.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-debuild -us -uc -b
-dh_clean
+# should use pristine-tar here
+ARCHIVE=nivlheim_1.9.3.orig.tar.gz
+rm -f ${ARCHIVE}
+tar -zcf ${ARCHIVE} * .git
+
+mkdir build
+cd build
+tar -zvxf ../${ARCHIVE}
+debuild -us -uc
+cd ..
+rm -rf build
