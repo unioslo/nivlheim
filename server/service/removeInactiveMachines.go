eb929bce6aea61f6630135db17b573924a9bfc46
Øyvind Hagberg <oyvind.hagberg@gmail.com>
2019-03-31 20:57:54 +0200

    Bugfix: It never removed files from gone machines
    Fix a bug where removeInactiveMachinesJob would actually never remove
    old files from the database if a machine was gone.
    Fix a bug in index.html, it had a superflous end-of-script tag.
    Fix a bug in processarchive, it would not set the received timestamp
    if a file was unchanged.

--- server/service/removeInactiveMachines.go
+++ server/service/removeInactiveMachines.go
@@ -19,6 +19,7 @@ func (job removeInactiveMachinesJob) HowOften() time.Duration {
 }
 
 func (job removeInactiveMachinesJob) Run(db *sql.DB) {
+	// Archive machines (delete the hostinfo entry, but keep the files)
 	rows, err := db.Query("SELECT certfp,extract(day from now()-lastseen) FROM hostinfo")
 	if err != nil {
 		log.Print(err)
@@ -32,20 +33,10 @@ func (job removeInactiveMachinesJob) Run(db *sql.DB) {
 		err = rows.Scan(&certfp, &days64)
 		if err != nil {
 			log.Print(err)
-			return
+			break
 		}
 		days := int(days64.Int64)
-		if days >= deleteDayLimit {
-			err = utility.RunStatementsInTransaction(db, []string{
-				"DELETE FROM hostinfo WHERE certfp=$1",
-				"DELETE FROM files WHERE certfp=$1",
-			}, certfp)
-			if err != nil {
-				log.Print(err)
-			} else {
-				dcount++
-			}
-		} else if days >= archiveDayLimit {
+		if days >= archiveDayLimit {
 			err = utility.RunStatementsInTransaction(db, []string{
 				"UPDATE files SET current=false WHERE certfp=$1",
 				"DELETE FROM hostinfo WHERE certfp=$1",
@@ -57,9 +48,38 @@ func (job removeInactiveMachinesJob) Run(db *sql.DB) {
 			}
 		}
 	}
+	if err = rows.Err(); err != nil {
+		log.Print(err)
+	}
+	rows.Close()
+	
+	// Delete old files where I haven't heard from the machine in a long time
+	rows, err = db.Query("SELECT DISTINCT certfp FROM files GROUP BY certfp"+
+		" HAVING max(received) < now() - interval '$1 days'", deleteDayLimit)
 	if err != nil {
 		log.Print(err)
 		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var certfp string
+		err = rows.Scan(&certfp)
+		if err != nil {
+			log.Print(err)
+			break
+		}
+		err = utility.RunStatementsInTransaction(db, []string{
+			"DELETE FROM hostinfo WHERE certfp=$1",
+			"DELETE FROM files WHERE certfp=$1",
+		}, certfp)
+		if err != nil {
+			log.Print(err)
+		} else {
+			dcount++
+		}
+	}
+	if err = rows.Err(); err != nil {
+		log.Print(err)
 	}
 	if acount > 0 || dcount > 0 {
 		log.Printf("Archived %d machines, deleted %d machines", acount, dcount)
