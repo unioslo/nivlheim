24a6c498ac836eeaaa84229af6f42c3950a8785c
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2019-11-26 15:25:46 +0100

    Duplicates of hosts should be archived not deleted
    If a host gets re-installed or otherwise loses its certificate,
    it will get a new one right away. The old "copy" remains in
    Nivlheim, and should be removed.
    But in case of reinstall we want to keep the old files archived for a
    while.

--- server/service/removeInactiveMachines.go
+++ server/service/removeInactiveMachines.go
@@ -15,7 +15,7 @@ func init() {
 }
 
 func (job removeInactiveMachinesJob) HowOften() time.Duration {
-	return time.Hour * 6
+	return time.Minute * 19
 }
 
 func (job removeInactiveMachinesJob) Run(db *sql.DB) {
@@ -27,34 +27,48 @@ func (job removeInactiveMachinesJob) Run(db *sql.DB) {
 		}
 	}()
 
-	// Archive machines (delete the hostinfo entry, but keep the files)
+	// How many days to wait before archiving a host
 	archiveDayLimit := config.ArchiveDayLimit
 	if archiveDayLimit == 0 {
 		archiveDayLimit = 30 // default value is 30 days
 	}
-	rows, err := db.Query("SELECT certfp,extract(day from now()-lastseen) FROM hostinfo")
+
+	// Find hostinfo rows that are old enough to be archived
+	const query1 = "SELECT certfp FROM hostinfo" +
+		" WHERE lastseen < now() - $1 * interval '1 days'"
+
+	// Find hostinfo rows that are duplicates of the same machine.
+	// This often happens if a machine is re-installed.
+	// It can also happen if the certificate files are deleted somehow.
+	const query2 = "SELECT certfp FROM hostinfo h, " +
+		" (SELECT DISTINCT ipaddr,serialno,MAX(lastseen) FROM hostinfo " +
+		" WHERE serialno is not null AND ipaddr is not null " +
+		" GROUP BY ipaddr,serialno HAVING count(*)>1) AS foo " +
+		" WHERE h.ipaddr=foo.ipaddr AND h.serialno=foo.serialno " +
+		" AND h.lastseen<foo.max"
+
+	// Archive the machines (delete the hostinfo entry, but keep the files)
+	rows, err := db.Query(query1+" UNION "+query2, archiveDayLimit)
 	if err != nil {
 		log.Panic(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var certfp string
-		var days64 sql.NullInt64
-		err = rows.Scan(&certfp, &days64)
+		err = rows.Scan(&certfp)
 		if err != nil {
 			log.Panic(err)
 		}
-		days := int(days64.Int64)
-		if days >= archiveDayLimit {
-			err = utility.RunStatementsInTransaction(db, []string{
-				"UPDATE files SET current=false WHERE certfp=$1",
-				"DELETE FROM hostinfo WHERE certfp=$1",
-			}, certfp)
-			if err != nil {
-				log.Panic(err)
-			} else {
-				acount++
-			}
+		err = utility.RunStatementsInTransaction(db, []string{
+			"UPDATE files SET current=false WHERE certfp=$1",
+			"DELETE FROM hostinfo WHERE certfp=$1",
+		}, certfp)
+		if err != nil {
+			log.Panic(err)
+		} else {
+			// These files should no longer show up in searches
+			removeHostFromFastSearch(certfp)
+			acount++
 		}
 	}
 	if err = rows.Err(); err != nil {
@@ -62,24 +76,14 @@ func (job removeInactiveMachinesJob) Run(db *sql.DB) {
 	}
 	rows.Close()
 
-	// Delete old files and machines
+	// Delete old files and machines that I haven't heard from in a long time
 	deleteDayLimit := config.DeleteDayLimit
 	if deleteDayLimit == 0 {
 		deleteDayLimit = 180 // default value is 180 days
 	}
 	rows, err = db.Query(
-		// Delete machines where I haven't heard from the machine in a long time
 		"SELECT DISTINCT certfp FROM files GROUP BY certfp"+
-			" HAVING max(received) < now() - $1 * interval '1 days'"+
-
-			// Delete machines that we've stopped hearing from, and
-			// since then another machine has taken over the ip address and hostname.
-			// This is most likely a machine that got re-installed, decommissioned,
-			// or lost its certificate.
-			" UNION "+
-			"SELECT certfp FROM hostinfo WHERE hostname is null "+
-			"AND lastseen < now() - interval '6 hours' "+
-			"AND ipaddr IN (select ipaddr from hostinfo where hostname is not null)",
+			" HAVING max(received) < now() - $1 * interval '1 days'",
 		deleteDayLimit)
 	if err != nil {
 		log.Panic(err)
@@ -98,6 +102,7 @@ func (job removeInactiveMachinesJob) Run(db *sql.DB) {
 		if err != nil {
 			log.Panic(err)
 		} else {
+			removeHostFromFastSearch(certfp)
 			dcount++
 		}
 	}
