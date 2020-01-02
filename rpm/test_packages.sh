bb298d0d8a57ce79731200ef290242659da3ddfc
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2020-01-02 17:13:43 +0100

    Don't setup Postgres during installation
    - Remove the Postgres setup code from setup.sh
    - Add code for setting up a dummy test database to test_packages.sh
    - installdb.sh reads database connection options from server.conf
    
    This commit is work-in-progress on #132.

--- rpm/test_packages.sh
+++ rpm/test_packages.sh
@@ -5,17 +5,20 @@
 # Install the packages. Different methods on Fedora and CentOS.
 if [ -f /etc/fedora-release ]; then
 	sudo dnf copr -y enable oyvindh/Nivlheim-test
-	sudo dnf install -y nivlheim-client nivlheim-server || touch installerror
+	echo "Installing Nivlheim server and client..."
+	sudo dnf install -y -q nivlheim-client nivlheim-server || touch installerror
 elif [ -f /etc/centos-release ]; then
 	sudo yum install -y epel-release
 	sudo curl -sS --retry 10 -o /etc/yum.repos.d/oyvindh-Nivlheim-test-epel-7.repo \
 		https://copr.fedorainfracloud.org/coprs/oyvindh/Nivlheim-test/repo/epel-7/oyvindh-Nivlheim-test-epel-7.repo
-	sudo yum install -y nivlheim-client nivlheim-server || touch installerror
+	echo "Installing Nivlheim server and client..."		
+	sudo yum install -y -q nivlheim-client nivlheim-server || touch installerror
 fi
 if [ -f installerror ]; then
 	echo "Package installation failed."
 	exit 1
 fi
+echo "Package installation completed."
 
 # If restorecon would change anything, it means something wasn't installed right
 sudo restorecon -nvR /var/www/nivlheim /var/log/nivlheim > /tmp/changed.log
@@ -24,6 +27,57 @@ if [[ $(cat /tmp/changed.log | wc -l) -gt 0 ]]; then
 	echo "There could be a problem with semanage commands in setup.sh."
 	exit 1
 fi
+
+# Stop Nivlheim while we mess with the database
+sudo systemctl stop nivlheim
+
+# Install and set up a local Postgres database
+sudo yum install -y postgresql postgresql-server || touch installerror
+if [ -f installerror ]; then
+	echo "Postgres installation failed."
+	exit 1
+fi
+if ! (sudo /usr/bin/postgresql-setup initdb || sudo /usr/bin/postgresql-setup --initdb); then
+	echo "Unable to initialize PostgreSQL database. Is there an existing installation?"
+	exit 1
+fi
+
+# Enable password-based authentication in Postgres, instead of the default
+sudo sed -i 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf
+
+# Start postgres
+sudo systemctl start postgresql
+
+# Create a database and user
+sudo -u postgres psql <<EOT
+CREATE USER testuser;
+ALTER USER testuser PASSWORD 'testpassword';
+CREATE DATABASE testdb WITH OWNER testuser;
+EOT
+echo "Created a database and a user."
+
+# Set the database connection parameters in server.conf
+# First remove any default settings
+grep -ive "^pg" /etc/nivlheim/server.conf > /tmp/server.conf \
+	&& sudo mv /tmp/server.conf /etc/nivlheim/
+# Then add settings
+cat <<EOT | sudo tee -a /etc/nivlheim/server.conf > /dev/null
+pghost=127.0.0.1
+pgport=5432
+pgdatabase=testdb
+pguser=testuser
+pgpassword=testpassword
+pgsslmode=disable
+EOT
+echo "------- /etc/nivlheim/server.conf -------"
+cat /etc/nivlheim/server.conf
+echo "-----------------------------------------"
+
+# Patch up the database
+/var/nivlheim/installdb.sh
+
+# Start nivlheim because we stopped it earlier in the script
+sudo systemctl start nivlheim
 
 # Give the system service a few seconds to start up
 sleep 5
@@ -63,15 +117,30 @@ fi
 # Turn on debug logging
 sudo sed -i.bak s/log4perl.logger.reqcert=INFO/log4perl.logger.reqcert=DEBUG/g /var/www/nivlheim/log4perl.conf
 # Configure the client to use the server at localhost
-echo "server=localhost" | sudo tee -a /etc/nivlheim/client.conf
+echo "server=localhost" | sudo tee -a /etc/nivlheim/client.conf >/dev/null
 # Run the client, it will be put on waiting list for a certificate
+echo "Running the client, it will be put on waiting list for a certificate"
 sudo /usr/sbin/nivlheim_client
 # Approve the client, using the API
+echo "Approving the client by using the API"
 ID=`curl -sS 'http://localhost:4040/api/v2/manualApproval?fields=approvalId'|perl -ne 'print $1 if /"approvalId":\s+(\d+)/'`
+if [[ "$ID" == "" ]]; then
+	echo "Error: The client wasn't on the waiting list."
+	echo "------- access_log: --------"
+	sudo tail -10 /var/log/httpd/access_log
+	echo "------- error_log: ---------"
+	sudo tail -10 /var/log/httpd/error_log
+	echo "------- system.log: --------"
+	tail -10 /var/log/nivlheim/system.log
+	echo "------- journal: -..--------"
+	sudo journalctl -S yesterday | grep nivlheim
+	exit 1
+fi
 curl -sSX PATCH --data "hostname=abcdef&approved=true" "http://localhost:4040/api/v2/manualApproval/$ID"
 
 # Run the client again, this time it will receive a certificate
 # and post data into the system
+echo "Running the client again, this time it will get a certificate"
 sudo /usr/sbin/nivlheim_client
 if [ ! -f /var/nivlheim/my.crt ]; then
 	echo "Certificate generation failed."
--- server/database/installdb.sh
+++ server/database/installdb.sh
@@ -3,8 +3,26 @@
 # halt on errors
 set -e
 
+# read server.conf and set env vars for database connection parameters
+if [[ -r "/etc/nivlheim/server.conf" ]]; then
+	# grep out the postgres config options and make the names upper case
+	grep -ie "^pg" /etc/nivlheim/server.conf | sed -e 's/\(.*\)=/\U\1=/' > /tmp/dbconf
+	source /tmp/dbconf
+	rm /tmp/dbconf
+else
+	echo "Unable to read server.conf"
+	exit 1
+fi
+export PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD
+
 # go to the script directory, where the sql files are
 cd $(dirname $0)
+
+# Verify that I am able to connect to the database
+if ! psql -X -w -c "SELECT version()" >/dev/null; then
+	echo "Unable to connect to the database"
+	exit 1
+fi
 
 # Determine the current patch level
 PATCHLEVEL=0
