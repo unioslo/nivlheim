37ec6c5d053ef3903a4873861f42f5a2324fc537
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2021-10-13 18:48:15 +0200

    Remove tty requirement and fix more tests

--- tests/test_cert_handling.sh
+++ tests/test_cert_handling.sh
@@ -30,7 +30,7 @@ curl -sS -X POST 'http://localhost:4040/api/v2/settings/ipranges' -d 'ipRange=10
 docker volume rm clientvar -f > /dev/null
 
 # Remove any previous cert files on the server
-docker exec -it docker_nivlheimweb_1 sh -c 'rm -f /var/www/nivlheim/certs/*'
+docker exec docker_nivlheimweb_1 sh -c 'rm -f /var/www/nivlheim/certs/*'
 
 # Run the client. This will call reqcert and post
 echo "Running the client"
@@ -39,14 +39,14 @@ if ! docker run --rm --network host -v clientvar:/var nivlheimclient --debug >/t
 	echo "--------------------------------------------"
 	cat /tmp/output
 	echo "access_log: --------------------------------"
-	docker exec -it docker_nivlheimweb_1 cat /var/log/httpd/access_log
+	docker exec docker_nivlheimweb_1 cat /var/log/httpd/access_log
 	echo "error_log: ---------------------------------"
-	docker exec -it docker_nivlheimweb_1 cat /var/log/httpd/error_log
+	docker exec docker_nivlheimweb_1 cat /var/log/httpd/error_log
     exit 1
 fi
 
 # Verify that reqcert didn't leave any files
-OUTPUT=$(docker exec -it docker_nivlheimweb_1 ls -1 /var/www/nivlheim/certs)
+OUTPUT=$(docker exec -t docker_nivlheimweb_1 ls -1 /var/www/nivlheim/certs)
 if [[ "$OUTPUT" != "" ]]; then
 	echo "Certificate files are left after reqcert:"
 	echo $OUTPUT
@@ -106,7 +106,7 @@ if [[ "$chain" != "$expect" ]]; then
 	echo "Details:"
 	$PSQL -c "SELECT certid,issued,first,previous,fingerprint FROM certificates ORDER BY certid"
 	echo "================= httpd access log:  =================="
-	docker exec -it docker_nivlheimweb_1 tail -20 /var/log/httpd/access_log
+	docker exec docker_nivlheimweb_1 tail -20 /var/log/httpd/access_log
 	echo "================= client output (1st time): ==========="
 	cat /tmp/first
 	echo "================= client output (2nd time): ==========="
@@ -127,7 +127,7 @@ if ! grep -q 'ghijkl' $tempdir/grepout; then
 fi
 
 # Verify that renewcert didn't leave any files
-OUTPUT=$(docker exec -it docker_nivlheimweb_1 ls -1 /var/www/nivlheim/certs)
+OUTPUT=$(docker exec -t docker_nivlheimweb_1 ls -1 /var/www/nivlheim/certs)
 if [[ "$OUTPUT" != "" ]]; then
 	echo "Certificate files are left after renewcert:"
 	echo $OUTPUT
@@ -137,20 +137,20 @@ fi
 # Blacklist and check response
 $PSQL -q -c "UPDATE certificates SET revoked=true"
 # Test ping
-if docker run --rm -v clientvar:/var --entrypoint curl nivlheimclient -skf --cert /var/nivlheim/my.crt --key /var/nivlheim/my.key \
+if docker run --rm -v clientvar:/var --network host --entrypoint curl nivlheimclient -skf --cert /var/nivlheim/my.crt --key /var/nivlheim/my.key \
 	https://localhost/cgi-bin/secure/ping; then
 	echo "Secure/ping worked even though cert was blacklisted."
 	exit 1
 fi
 # Test post (it will get a 403 anyway, because the nonce is missing)
-docker run --rm -v clientvar:/var --entrypoint curl nivlheimclient -sk --cert /var/nivlheim/my.crt --key /var/nivlheim/my.key \
+docker run --rm -v clientvar:/var --network host --entrypoint curl nivlheimclient -sk --cert /var/nivlheim/my.crt --key /var/nivlheim/my.key \
 	https://localhost/cgi-bin/secure/post > $tempdir/postresult || true
 if ! grep -qi "revoked" $tempdir/postresult; then
 	echo "Post worked even though cert was blacklisted."
 	exit 1
 fi
 # Test renew
-docker run --rm -v clientvar:/var --entrypoint curl nivlheimclient -sk --cert /var/nivlheim/my.crt --key /var/nivlheim/my.key \
+docker run --rm -v clientvar:/var --network host --entrypoint curl nivlheimclient -sk --cert /var/nivlheim/my.crt --key /var/nivlheim/my.key \
 	https://localhost/cgi-bin/secure/renewcert > $tempdir/renewresult || true
 if ! grep -qi "revoked" $tempdir/renewresult; then
 	echo "Renewcert worked even though cert was blacklisted."
@@ -158,13 +158,13 @@ if ! grep -qi "revoked" $tempdir/renewresult; then
 fi
 
 # Check logs for errors
-if docker exec -it docker_nivlheimapi_1 grep -A1 "ERROR" /var/log/nivlheim/system.log; then
+if docker exec -t docker_nivlheimweb_1 grep -A1 "ERROR" /var/log/nivlheim/system.log; then
     exit 1
 fi
-if docker exec -it docker_nivlheimapi_1 journalctl -u nivlheim | grep -i error; then
+if docker logs docker_nivlheimapi_1 2>&1 | grep -i error; then
     exit 1
 fi
-if docker exec -it docker_nivlheimapi_1 grep "cgi:error" /var/log/httpd/error_log | grep -v 'random state'; then
+if docker exec -t docker_nivlheimweb_1 grep "cgi:error" /var/log/httpd/error_log | grep -v 'random state'; then
     exit 1
 fi
 
