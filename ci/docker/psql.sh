11c0897e701c54c95565a95f53b5e0971bd8d765
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2021-10-13 22:30:47 +0200

    Edit psql.sh to not be interactive if a command is supplied

--- ci/docker/psql.sh
+++ ci/docker/psql.sh
@@ -1,1 +1,6 @@
-docker run -it --rm --network docker_default -e "PGPASSWORD=notsecret" postgres psql -h postgres -U nivlheim nivlheim "$@"
+#!/bin/bash
+I="-i "
+if [[ "$*" == *"-c"* ]]; then
+    I=""
+fi
+docker run $I -t --rm --network docker_default -e "PGPASSWORD=notsecret" postgres psql -h postgres -U nivlheim nivlheim "$@"
