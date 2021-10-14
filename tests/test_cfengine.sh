cc4ddb3d624a444ce30777cf7b4863d05393c3cd
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2021-10-14 12:36:13 +0200

    Test bootstrapping with a CFEngine certificate

--- tests/test_cfengine.sh
+++ tests/test_cfengine.sh
@@ -2,108 +2,89 @@
 
 echo "------------- Test bootstrapping with a CFEngine certificate ------------"
 set -e
+cd `dirname $0`
+PSQL=../ci/docker/psql.sh
 
-# Clean/init everything
-sudo systemctl stop nivlheim
-sudo rm -f /var/log/nivlheim/system.log /var/nivlheim/my.{crt,key} \
-	/var/run/nivlheim_client_last_run /var/www/nivlheim/certs/* \
-	/var/www/nivlheim/queue/*
-echo -n | sudo tee /var/log/httpd/error_log
-/var/nivlheim/installdb.sh --wipe
-
-# Configure the CFengine key dir setting in the server config
-if ! grep -s -e "^CFEngineKeyDir" /etc/nivlheim/server.conf > /dev/null; then
-    echo "CFEngineKeyDir=/var/cfekeys" | sudo tee -a /etc/nivlheim/server.conf
-fi
-
-# Start the server process again
-sudo systemctl start nivlheim
-sleep 4
-
-# Read database connection options from server.conf and set ENV vars for psql
-if [[ -r "/etc/nivlheim/server.conf" ]]; then
-	# grep out the postgres config options and make the names upper case
-	grep -ie "^pg" /etc/nivlheim/server.conf | sed -e 's/\(.*\)=/\U\1=/' > /tmp/dbconf
-	source /tmp/dbconf
-	rm /tmp/dbconf
-else
-	echo "Unable to read server.conf"
-	exit 1
-fi
-export PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD
-
-# Configure the server setting in the client config
-if ! grep -s -e "^server" /etc/nivlheim/client.conf > /dev/null; then
-	echo "server=localhost" | sudo tee -a /etc/nivlheim/client.conf >/dev/null
-fi
+# Configure where reqcert will look for CFEngine keys
+docker exec docker_nivlheimweb_1 sh -c 'echo "CFEngineKeyDir=/var/cfekeys" >> /etc/nivlheim/server.conf'
 
 # Try to run the client without CFEngine signature or any form of pre-approval.
 # Should result in it being put on the waiting list.
 echo "Running the client without any trust"
-sudo /usr/sbin/nivlheim_client --nocfe || true
-A=$(psql --no-align -t -c "SELECT count(*) FROM waiting_for_approval")
+OUTPUT=$(docker run --rm --network host -v clientvar:/var nivlheimclient --nocfe --debug 2>&1) || true
+A=$($PSQL --no-align -t -c "SELECT count(*) FROM waiting_for_approval")
 if [[ "$A" == "0" ]]; then
 	echo "The client should have been put on the waiting list, but wasn't..."
+	echo $OUTPUT
 	exit 1
 fi
 # Clean up
-psql -X --no-align -t -q -c "TRUNCATE TABLE waiting_for_approval"
+$PSQL -X --no-align -t -q -c "TRUNCATE TABLE waiting_for_approval"
 
-# If CFEngine isn't installed on this machine, install a fake CFEngine key pair
-FAKE=0
-if [[ ! -d /var/cfengine ]]; then
-	echo "Installing a fake CFEngine key pair for this host"
-	FAKE=1
-	# This copy will be used by the client
-	sudo mkdir -p /var/cfengine/ppkeys
-	sudo cp cfengine.priv /var/cfengine/ppkeys/localhost.priv
-	sudo cp cfengine.pub /var/cfengine/ppkeys/localhost.pub
-	# and this copy will be used by the server
-	sudo mkdir -p /var/cfekeys
-	sudo cp cfengine.pub /var/cfekeys/root-MD5=01234567890123456789012345678932.pub   # default value for a machine without cf-key
-	# Ensure the httpd process will have read access.
-	# This will probably be handled differently on the actual server.
-	sudo chmod -R go+r /var/cfekeys
-	sudo chcon -R -t httpd_sys_content_t /var/cfekeys
-fi
+# Install a fake CFEngine key pair on a client container
+docker run --rm -v clientvar:/var --entrypoint sh nivlheimclient -c 'mkdir -p /var/cfengine/ppkeys'
+docker rm banana >/dev/null 2>&1 || true
+docker create --name banana --network host -v clientvar:/var nivlheimclient --debug
+docker cp cfengine.priv banana:/var/cfengine/ppkeys/localhost.priv
+docker cp cfengine.pub banana:/var/cfengine/ppkeys/localhost.pub
+# and the public key will also be used by the server
+docker exec docker_nivlheimweb_1 mkdir -p /var/cfekeys
+docker cp cfengine.pub docker_nivlheimweb_1:/var/cfekeys/root-MD5=01234567890123456789012345678932.pub   # default value for a machine without cf-key
+# Ensure the httpd process will have read access
+docker exec docker_nivlheimweb_1 chmod -R go+r /var/cfekeys
 
-# Configure where reqcert will look for CFEngine keys
-sudo sed -i 's!CFEngineKeyDir=.*!CFEngineKeyDir=/var/cfekeys!i' /etc/nivlheim/server.conf
+function printlogs() {
+	echo "------- access_log -------------------------------"
+	docker exec docker_nivlheimweb_1 grep -v 127.0.0.1 /var/log/httpd/access_log || true
+	echo "------- error_log --------------------------------"
+	docker exec docker_nivlheimweb_1 grep "cgi:error" /var/log/httpd/error_log || true
+	echo "------- system.log--------------------------------"
+	docker exec docker_nivlheimweb_1 cat /var/log/nivlheim/system.log || true
+	echo "------- docker logs ------------------------------"
+	docker logs docker_nivlheimapi_1 || true
+}
 
 # Run the client. This will call reqcert and post.
 # Note that no ip address ranges have been registered on the server,
 # so the client will have to rely on its CFEngine key to gain trust.
-echo "Running the client"
-sudo /usr/sbin/nivlheim_client --debug || true
-
-if [[ "$FAKE" == "1" ]]; then
-	echo "Removing the CFEngine keys"
-	sudo rm -rf /var/cfengine/ppkeys
-fi
-
-if [[ ! -f /var/run/nivlheim_client_last_run ]]; then
-    echo "The client failed to post data successfully."
-	cat /var/log/nivlheim/system.log
-	sudo grep "cgi:error" /var/log/httpd/error_log
-	sudo ausearch -i -ts recent -sv no --input-logs
-    exit 1
-fi
-
-# The client should have received a certificate
-if [[ ! -f /var/nivlheim/my.crt ]]; then
-	echo "The client didn't receive a certificate."
+echo "Running the client, using its CFEngine key this time"
+if ! docker start -a banana >/tmp/output 2>&1; then
+	echo "The client failed to post data successfully."
+	echo "---------- client output ----------------------"
+	cat /tmp/output
+	printlogs
 	exit 1
 fi
+if ! grep -q "CFEngine signature" /tmp/output; then
+	echo "The client did not use its CFEngine key."
+	echo "---------- client output ----------------------"
+	cat /tmp/output
+	printlogs
+	exit 1
+fi
+rm -f /tmp/output
+
+# The client should have received a certificate
+rm -f /tmp/my.crt
+docker cp banana:/var/nivlheim/my.crt /tmp
+if [[ ! -f /tmp/my.crt ]]; then
+	echo "The client didn't receive a certificate."
+	printlogs
+	exit 1
+fi
+rm /tmp/my.crt
 
 # The certificate should have the field trusted_by_cfengine = true in the database
 echo "Verifying the certificate in the database"
-trust=$(psql --no-align -t -c "SELECT trusted_by_cfengine FROM certificates")
+trust=$($PSQL --no-align -t -c "SELECT trusted_by_cfengine FROM certificates" | tr -d '\r\n')
 if [[ "$trust" != "t" ]]; then
 	echo "trusted_by_cfengine isn't true in the database table."
+	printlogs
 	exit 1
 fi
 
 # Wait for the files to be parsed
+echo "Wait for the files to be parsed..."
 sleep 20
 
 # Trigger the job that gives the machine a hostname
@@ -115,10 +96,10 @@ sleep 5
 
 # The host should have a hostname now
 echo "Looking for the hostname in the database"
-names=$(psql --no-align -t -c "SELECT count(*) FROM hostinfo WHERE hostname IS NOT NULL")
+names=$($PSQL --no-align -t -c "SELECT count(*) FROM hostinfo WHERE hostname IS NOT NULL" | tr -d '\r\n')
 if [[ "$names" -lt "1" ]]; then
 	echo "The host didn't get a name in Nivlheim."
-	journalctl -u nivlheim
+	printlogs
 	exit 1
 fi
 
