4671d92c6eb9969bec88cc7c5b730ea8f5390d78
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2021-11-01 13:29:06 +0100

    Fix tag in build.sh + "docker inspect" after build

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`/../..  # cd to the root of the git repo
-docker build -f ci/docker/Dockerfile --tag nivlheimwww:latest .
+docker build -f ci/docker/Dockerfile --tag nivlheim-www:latest .
