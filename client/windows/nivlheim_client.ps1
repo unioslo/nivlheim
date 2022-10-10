8a7430b281ada131d5aaefd0bf58491f94e7ee85
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2022-10-10 13:57:54 +0200

    Bump version

--- client/windows/nivlheim_client.ps1
+++ client/windows/nivlheim_client.ps1
@@ -31,7 +31,7 @@ param(
 	[bool]$nosleep = $false
 )
 
-Set-Variable version -option Constant -value "2.7.14"
+Set-Variable version -option Constant -value "2.7.15"
 Set-Variable useragent -option Constant -value "NivlheimPowershellClient/$version"
 Set-PSDebug -strict
 Set-StrictMode -version "Latest"	# http://technet.microsoft.com/en-us/library/hh849692.aspx
