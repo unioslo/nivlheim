043cb26a61241d8c6de1252ee4b8c695c1ba4b44
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2024-07-31 14:52:28 +0200

    Make psql.sh support podman

--- ci/docker/psql.sh
+++ ci/docker/psql.sh
@@ -3,4 +3,9 @@ I="-it "
 if [[ "$*" == *"-c"* ]]; then
     I=""
 fi
-docker exec $I -e PGCONNECT_TIMEOUT=30 docker_postgres_1 psql -U nivlheim -h localhost -d nivlheim "$@"
+EXECUTABLE=docker
+if ! type docker >/dev/null 2>&1; then
+    EXECUTABLE=podman
+fi
+CONTAINER_NAME=$($EXECUTABLE ps --filter ancestor=postgres --format '{{.Names}}')
+$EXECUTABLE exec $I -e PGCONNECT_TIMEOUT=30 $CONTAINER_NAME psql -U nivlheim -h localhost -d nivlheim "$@"
