ef2dd2aedf8f6ddd373e922daea1b545b383c403
Øyvind Hagberg <oyvind.hagberg@usit.uio.no>
2025-06-11 13:14:29 +0200

    Put client in /usr/bin for compat. with Fedora 42+

--- debian/nivlheim-client.cron.d
+++ debian/nivlheim-client.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the nivlheim-client package
 #
-*/5 * * * *  root  [ -x /usr/sbin/nivlheim_client ] && /usr/sbin/nivlheim_client -minperiod 3600 -sleeprandom 300 > /dev/null
+*/5 * * * *  root  [ -x /usr/bin/nivlheim_client ] && /usr/bin/nivlheim_client -minperiod 3600 -sleeprandom 300 > /dev/null
